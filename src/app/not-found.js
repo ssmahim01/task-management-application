c4cd2cb00091982ab069be992d1917e41ab7d657
ssmahim01 <mahimzayn666@gmail.com>
2025-02-05 23:43:58 +0600

    Added loading file for Home page and update-task page with other two pages also Updated not-found page

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -4,20 +4,22 @@ import { IoIosReturnLeft } from "react-icons/io";
 
 export default function NotFound() {
     return (
-        <div className="py-20 flex flex-col justify-center items-center">
-            <figure className="w-full h-full pb-5">
+        <div className="pt-10 pb-16">
+            <figure className="flex justify-center items-center">
                 <Image
                     src="/404.gif"
                     alt="GIF of error page"
-                    width={320}
-                    height={280}
+                    width={550}
+                    height={550}
                 />
             </figure>
 
-            <h2 className="md:text-4xl text-3xl text-gray-900 font-extrabold pb-4">Not Found Page: 404</h2>
-            <p className="text-xl text-gray-600 font-semibold pb-4">Could not find requested resource</p>
+           <div className="space-y-3 -mt-8 flex flex-col justify-center items-center">
+           <h2 className="md:text-4xl text-3xl text-gray-900 font-extrabold">Not Found Page: 404</h2>
+            <p className="text-xl text-gray-600 font-semibold">Could not find requested resource</p>
 
-            <Link href="/"><button className="btn btn-outline rounded-full px-10 border border-gray-300 bg-white/60 shadow-sm font-bold flex gap-2 items-center"><IoIosReturnLeft className="text-2xl" /> <span className="text-lg">Return Home</span> </button></Link>
+            <Link href="/"><button className="mt-3 btn btn-outline rounded-full px-10 border border-gray-300 bg-white/60 shadow-sm font-bold flex gap-2 items-center"><IoIosReturnLeft className="text-2xl" /> <span className="text-lg">Return Home</span> </button></Link>
+           </div>
         </div>
     )
 }
---
+++ src/app/update-task/loading.js
@@ -0,0 +1,7 @@
+export default function Loading() {
+    return (
+        <div className="flex justify-center items-center lg:pt-56 pt-40">
+            <span className="loading-lg loading loading-spinner text-success"></span>
+        </div>
+    )
+}
